4d1d17b2da0300dc6344cfa1d729c8aa6da3fbbf
DESKTOP-OJ1GR5E\HP <kasimoluwasegun@gmail.com>
2022-06-18 21:09:23 +0100

    added tutor Dao

---
+++ src/main/java/com/SMS/SMSapi/dao/TutorDao.java
@@ -0,0 +1,7 @@
+package com.SMS.SMSapi.dao;
+
+import com.SMS.SMSapi.model.entities.Tutor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TutorDao extends CrudRepository<Tutor,Long> {
+}
