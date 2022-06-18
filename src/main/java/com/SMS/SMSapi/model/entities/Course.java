8ddf1cdca2183dad693b6b1b5251be400c81da55
DESKTOP-OJ1GR5E\HP <kasimoluwasegun@gmail.com>
2022-06-18 20:48:34 +0100

    Added curriculum and tutor

--- src/main/java/com/SMS/SMSapi/model/entities/Course.java
+++ src/main/java/com/SMS/SMSapi/model/entities/Course.java
@@ -1,4 +1,27 @@
 package com.SMS.SMSapi.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String courseTitle;
+    private String courseDescription;
+
 }
